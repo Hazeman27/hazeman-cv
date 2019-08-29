70b8b905791c36ea8411f0c33fbe2d5372a8807f
hazeman <tremul27@gmail.com>
2019-08-29 13:20:41 +0200

    Single page app with no animations

--- js/router.js
+++ js/router.js
@@ -18,8 +18,6 @@ export default class Router {
 
         this.container.innerHTML = responseText;
         this.importModule();
-
-        // this.animateContent(responseText);
     }
 
     async importModule() {
@@ -32,54 +30,5 @@ export default class Router {
         } catch (error) {
             console.log(error.message);
         }
-    }
-
-    animateContent(responseText, containerOpacityModifier = 5) {
-
-        let opacity = 100;
-
-        let fadeOutAnimationRequest = null;
-        let fadeInAnimationRequest = null;
-
-        const modifyContainerOpacity = (modifier) => {
-
-            this.container.style.opacity = opacity / 100;
-            opacity += modifier;
-        }
-
-        const fadeOut = () => {
-
-            if (opacity <= 0) {
-                
-                this.updateContent(responseText);
-                cancelAnimationFrame(fadeOutAnimationRequest);
-
-                fadeInAnimationRequest = requestAnimationFrame(fadeIn);
-                return;
-            }
-
-            modifyContainerOpacity(containerOpacityModifier * -1);
-            requestAnimationFrame(fadeOut);
-        };
-    
-        const fadeIn = () => {
-            
-            if (opacity >= 100 + containerOpacityModifier) {
-
-                cancelAnimationFrame(fadeInAnimationRequest);
-                return;
-            }
-
-            modifyContainerOpacity(containerOpacityModifier);
-            requestAnimationFrame(fadeIn);
-        };
-
-        fadeOutAnimationRequest = requestAnimationFrame(fadeOut);
-    }
-
-    updateContent(responseText) {
-
-        this.container.innerHTML = responseText;
-        this.importModule();
     }
 }
